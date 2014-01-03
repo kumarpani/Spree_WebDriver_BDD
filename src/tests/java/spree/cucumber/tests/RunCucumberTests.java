caddbd0967123f8efdad46f588992d9e3531ecb7
shivaling sannalli <shivaling.sannalli@RACKSPACE.COM>
2014-01-03 13:46:27 +0530

    git ignore .idea dir

---
+++ src/tests/java/spree/cucumber/tests/RunCucumberTests.java
@@ -0,0 +1,18 @@
+package src.tests.java.spree.cucumber.tests;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: panikumar
+ * Date: 03/12/13
+ * Time: 4:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = {"html:target/cucumber-html-report"})
+public class RunCucumberTests {
+}
